8adefd7cff630db73321dba9f1548c6142062f7d
Dulal-Mondal <dm640183@gmail.com>
2025-08-27 23:54:12 +0600

    call alert is done

--- script.js
+++ script.js
@@ -9,5 +9,32 @@ for (const heart of hearts) {
         heartIcon.textContent = heartCount;
     })
 
+}
 
+// code for call;
+
+const serviceCalls = document.getElementsByClassName('call-btn');
+const serviceNames = document.getElementsByClassName('service-name');
+const serviceNumbers = document.getElementsByClassName('service-number');
+const coinCount = document.getElementById('coins');
+
+for (let i = 0; i < serviceCalls.length; i++) {
+    serviceCalls[i].addEventListener('click', function () {
+
+        const name = serviceNames[i].innerText;
+        const number = serviceNumbers[i].innerText;
+
+
+        let coins = parseInt(coinCount.innerText);
+
+
+        if (coins < 20) {
+            alert("Not enough coins! You need at least 20 coins to make a call.");
+            return;
+        }
+        coins = coins - 20;
+        coinCount.innerText = coins;
+
+        alert("Calling " + name + "  " + number);
+    });
 }
